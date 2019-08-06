088a6adaa2fccc3fab760b72d73a51eac0c1ab1b
BIT <BIT@DESKTOP-7P1ENO8>
2019-08-06 21:34:58 +0900

    3,4,5번 완료

--- src/practice01/prob1/Prob1.java
+++ src/practice01/prob1/Prob1.java
@@ -16,6 +16,6 @@ public class Prob1 {
 		}else {
 			
 		}
-			
+		scanner.close();
 	}
 }
--- src/practice01/prob3/Prob3.java
+++ src/practice01/prob3/Prob3.java
@@ -1,32 +1,29 @@
 package practice01.prob3;
-
 import java.util.Scanner;
 
 public class Prob3 {
 	public static void main (String args[]) {
 		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(" 숫자를 입력하세요 : ");
 
-		while (true) {
-			System.out.println(" 숫자를 입력하세요 : ");
+		int number = scanner.nextInt();
+		int even = 0;
+		int odd = 0;
 
-			int number = scanner.nextInt();
-			int even = 0;
-			int odd = 0;
-
-			for (int i = 0; i <= number; i++ ) {
-				if (i % 2 == 0) {
-					even += i;
-				}else {
-					odd += i;
-				}
-			}
-			
-			if (number % 2 == 0 ) {
-				System.out.println(even);
+		for (int i = 0; i <= number; i++ ) {
+			if (i % 2 == 0) {
+				even += i;
 			}else {
-				System.out.println(odd);
+				odd += i;
 			}
 		}
-		
+
+		if (number % 2 == 0 ) {
+			System.out.println(even);
+		}else {
+			System.out.println(odd);
+		}
+		scanner.close();
 	}
 }
